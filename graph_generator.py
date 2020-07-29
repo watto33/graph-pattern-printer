91a4f5571fb13f019625679646cb2fbb396d3f77
Sriraam N <sriraamwatto33@gmail.com>
2020-07-29 18:09:11 +0530

    Working code with edge cases

--- graph_generator.py
+++ graph_generator.py
@@ -30,23 +30,32 @@ for i in str_input_series_arr:
 row_count = col_count = temp_count = max_column = 0
 
 # To identify the number of rows and columns.
-
+starting_pos = 0
 for i in range(len(num_input)):
-    col_count += num_input[i]
-    if i % 2 == 0:
-        temp_count += num_input[i]
+    if(num_input[i] < 0):
+        col_count += -num_input[i]
     else:
-        temp_count -= num_input[i]
+        col_count += num_input[i]
+    if i % 2 == 0:
+        temp_count = temp_count + num_input[i]
+    else:
+        temp_count = temp_count - num_input[i]
+    if temp_count < 0 and starting_pos > temp_count:
+        starting_pos = temp_count
     if(temp_count > row_count):
         row_count = temp_count
         max_column = col_count
+        temp_count = row_count
 
+starting_pos = -starting_pos
 '''
 Creating an empty 2D Array (Nested Lists) of size Row Count x Column Count.
 
 The extra one added to row count is to print the extra string to be displayed at top of max column.
 '''
+# print(row_count)
 
+row_count = row_count+starting_pos
 matrix = [[0 for x in range(col_count)] for y in range(row_count+1)]
 
 # Filling the array initially with spaces.
@@ -54,20 +63,36 @@ matrix = [[0 for x in range(col_count)] for y in range(row_count+1)]
 for i in range(row_count+1):
     for j in range(col_count):
         matrix[i][j] = ' '
+print("Neg: ", starting_pos)
+print("Row count: ", row_count)
 
-row_ctr = col_ctr = 0
+row_ctr = starting_pos
+col_ctr = 0
 
 # Storing the symbols corresponding to rows and columns
 
+# print(type(len(num_input)))
+
+temp_ctr = 0
+flag = 0
+
 for i in range(len(num_input)):
+    if num_input[i] < 0:
+        num_input[i] = -num_input[i]
+        temp_ctr += 1
+        flag = 1
     for j in range(num_input[i]):
-        if i % 2 == 1:
+        if temp_ctr % 2 == 1:
             row_ctr -= 1
             matrix[row_ctr][col_ctr] = '\\'
         else:
             matrix[row_ctr][col_ctr] = '/'
             row_ctr += 1
         col_ctr += 1
+    if flag == 1:
+        temp_ctr -= 1
+        flag = 0
+    temp_ctr += 1
 
 
 # Printing the matrix to display the graph
